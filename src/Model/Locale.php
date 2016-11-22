28a6fe0bd13133d0cad1994c1e2c56f10913edf8
Dirk Heyka <heyka@eosnewmedia.de>
2016-11-22 14:54:36 +0100

    Added Dispatch, LanguageSubShop, Locale and Shop (incl. Interfaces)

---
+++ src/Model/Locale.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types = 1);
+
+namespace Enm\ShopwareSdk\Model;
+
+/**
+ * @author Dirk Heyka <heyka@eosnewmedia.de>
+ */
+class Locale implements LocaleInterface
+{
+
+    /**
+     * @var
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var string
+     */
+    private $language;
+
+    /**
+     * @var string
+     */
+    private $territory;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     *
+     * @return LocaleInterface
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param string $locale
+     *
+     * @return LocaleInterface
+     */
+    public function setLocale(string $locale): LocaleInterface
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param string $language
+     *
+     * @return LocaleInterface
+     */
+    public function setLanguage(string $language): LocaleInterface
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTerritory(): string
+    {
+        return $this->territory;
+    }
+
+    /**
+     * @param string $territory
+     *
+     * @return LocaleInterface
+     */
+    public function setTerritory(string $territory): LocaleInterface
+    {
+        $this->territory = $territory;
+
+        return $this;
+    }
+}
