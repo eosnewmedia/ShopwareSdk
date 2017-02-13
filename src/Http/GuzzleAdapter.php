abd973e075d3ed790f30fa1af896f32d53632562
Nicklas Reincke <reincke@eosnewmedia.de>
2017-02-13 12:27:15 +0100

    Added threefold request attempt to bypass possible non-reproducible errors.

--- src/Http/GuzzleAdapter.php
+++ src/Http/GuzzleAdapter.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 
 /**
  * @author Philipp Marien <marien@eosnewmedia.de>
+ * @author Nicklas Reincke <reincke@eosnewmedia.de>
  */
 class GuzzleAdapter implements ClientInterface
 {
@@ -15,22 +16,22 @@ class GuzzleAdapter implements ClientInterface
      * @var string
      */
     private $baseUri = '';
-    
+
     /**
      * @var string
      */
     private $username = '';
-    
+
     /**
      * @var string
      */
     private $password = '';
-    
+
     /**
      * @var Guzzle
      */
     private $guzzleClient;
-    
+
     /**
      * @param Guzzle $guzzleClient
      */
@@ -38,7 +39,7 @@ class GuzzleAdapter implements ClientInterface
     {
         $this->guzzleClient = $guzzleClient;
     }
-    
+
     /**
      * @param string $baseUri
      * @param string $username
@@ -51,64 +52,72 @@ class GuzzleAdapter implements ClientInterface
         $this->baseUri  = $baseUri;
         $this->username = $username;
         $this->password = $password;
-        
+
         return $this;
     }
-    
+
     /**
      * @param string $path
      * @param array $query
      *
      * @return ResponseInterface
+     *
      * @throws \Exception
+     * @throws \InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function get(string $path, array $query = []): ResponseInterface
     {
         return $this->request('GET', $path, $query);
     }
-    
+
     /**
      * @param string $path
      * @param array $query
      * @param array $body
      *
      * @return ResponseInterface
+     *
      * @throws \Exception
+     * @throws \InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function post(string $path, array $query = [], array $body = []): ResponseInterface
     {
         return $this->request('POST', $path, $query, $body);
     }
-    
+
     /**
      * @param string $path
      * @param array $query
      * @param array $body
      *
      * @return ResponseInterface
+     *
      * @throws \Exception
+     * @throws \InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function put(string $path, array $query = [], array $body = []): ResponseInterface
     {
         return $this->request('PUT', $path, $query, $body);
     }
-    
+
     /**
      * @param string $path
      * @param array $query
      *
      * @return ResponseInterface
+     *
      * @throws \Exception
+     * @throws \InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function delete(string $path, array $query = []): ResponseInterface
     {
         return $this->request('DELETE', $path, $query);
     }
-    
+
     /**
      * @param string $method
      * @param string $path
@@ -116,47 +125,43 @@ class GuzzleAdapter implements ClientInterface
      * @param array $body
      *
      * @return ResponseInterface
+     *
      * @throws \Exception
+     * @throws \InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     private function request(string $method, string $path, array $query = [], array $body = []): ResponseInterface
     {
         $request = [
-          'base_uri'    => $this->baseUri,
-          'auth'        => [$this->username, $this->password, 'digest'],
-          'http_errors' => false,
+            'base_uri'    => $this->baseUri,
+            'auth'        => [$this->username, $this->password, 'digest'],
+            'http_errors' => false,
         ];
-        
+
         if (count($query) > 0) {
             $request['query'] = $query;
         }
+
         if (count($body) > 0) {
             $request['json'] = $body;
         }
-        
-        $response = $this->guzzleClient->request(
-          $method,
-          $path,
-          $request
-        );
-        
-        $this->handleResponse($response);
-        
-        return $response;
-    }
-    
-    /**
-     * @param ResponseInterface $response
-     *
-     * @return GuzzleAdapter
-     * @throws \Exception
-     */
-    private function handleResponse(ResponseInterface $response): GuzzleAdapter
-    {
-        if ($response->getStatusCode() >= 400) {
-            throw new \InvalidArgumentException((string)$response->getBody());
+
+        $response = null;
+
+        for ($i = 0; $i < 3; $i++) {
+
+            $response = $this->guzzleClient->request(
+                $method,
+                $path,
+                $request
+            );
+
+            if ($response->getStatusCode() < 400) {
+                return $response;
+            }
         }
-        
-        return $this;
+
+        throw new \InvalidArgumentException((string)$response->getBody());
     }
+
 }
